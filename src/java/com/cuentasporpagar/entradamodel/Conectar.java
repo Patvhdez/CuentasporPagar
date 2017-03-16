2e24cb0fbfaff05cd4d1535eec7b3ee86287c32c
Martin Edmundo <>
2017-03-16 12:36:14 -0400

    Se actualizó el proyecto para que apunte a la base de datos en la nube

--- src/java/com/cuentasporpagar/entradamodel/Conectar.java
+++ src/java/com/cuentasporpagar/entradamodel/Conectar.java
@@ -10,10 +10,9 @@ public class Conectar {
         
     DriverManagerDataSource dataSource = new DriverManagerDataSource();
     dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/CuentasDB");
-        dataSource.setUsername("root");
-        dataSource.setPassword("1234");
+        dataSource.setUrl("jdbc:mysql://us-cdbr-iron-east-03.cleardb.net:3306/heroku_9ee598636de48da");
+        dataSource.setUsername("bed5e0f367d4fa");
+        dataSource.setPassword("8c9fdf2b");
         return dataSource;
-    
     }
 }
--- src/java/com/cuentasporpagar/model/Conectar.java
+++ src/java/com/cuentasporpagar/model/Conectar.java
@@ -7,11 +7,11 @@ public class Conectar {
     
     public DriverManagerDataSource conectar(){
         
-        DriverManagerDataSource dataSource  = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/CuentasDB");
-        dataSource.setUsername("root");
-        dataSource.setPassword("1234");
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://us-cdbr-iron-east-03.cleardb.net:3306/heroku_9ee598636de48da");
+        dataSource.setUsername("bed5e0f367d4fa");
+        dataSource.setPassword("8c9fdf2b");
         return dataSource;
     }
     
